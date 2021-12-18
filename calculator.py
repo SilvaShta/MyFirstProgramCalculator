5625bb10c8612e692f40583660f4707413f11a9a
SilvaShta <suk4ik80@gmail.com>
2021-12-18 18:46:54 +0200

    Window for calculator

---
+++ calculator.py
@@ -0,0 +1,12 @@
+# This is a sample Python script.
+import tkinter as tk
+
+win = tk.Tk()
+win.title("Калькулятор")
+win.geometry("200x300")
+win.resizable(False, False)
+
+btnC = tk.Button(win,text='C')
+btnC.pacsk()
+
+win.mainloop()
